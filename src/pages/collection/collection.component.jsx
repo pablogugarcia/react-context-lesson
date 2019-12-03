6e0c02b5b21b88b2724ce40c2d4064a151821955
Pablo Garcia <pablo.garcia366@gmail.com>
2019-12-02 22:24:48 -0300

    lesson 1

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useContext } from "react";
+import CollectionItem from "../../components/collection-item/collection-item.component";
+import CollectionContext from "../../contexts/collections/collections";
+import "./collection.styles.scss";
 
-import CollectionItem from '../../components/collection-item/collection-item.component';
-
-import { selectCollection } from '../../redux/shop/shop.selectors';
-
-import './collection.styles.scss';
-
-const CollectionPage = ({ collection }) => {
+const CollectionPage = ({ match }) => {
+  const shopDataContext = useContext(CollectionContext);
+  const collection = shopDataContext[match.params.collectionId];
   const { title, items } = collection;
   return (
-    <div className='collection-page'>
-      <h2 className='title'>{title}</h2>
-      <div className='items'>
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
         {items.map(item => (
           <CollectionItem key={item.id} item={item} />
         ))}
@@ -20,9 +18,28 @@ const CollectionPage = ({ collection }) => {
     </div>
   );
 };
+/** Una manera de utilizar context Api */
+// const CollectionPage = ({ match }) => {
+//   //const shopDataContext = useContext(CollectionContext);
+//   // const { title, items } = collection;
+//   return (
+//     <CollectionContext.Consumer>
+//       {collections => {
+//         const collection = collections[match.params.collectionId];
+//         const { title, items } = collection;
+//         return (
+//           <div className="collection-page">
+//             <h2 className="title">{title}</h2>
+//             <div className="items">
+//               {items.map(item => (
+//                 <CollectionItem key={item.id} item={item} />
+//               ))}
+//             </div>
+//           </div>
+//         );
+//       }}
+//     </CollectionContext.Consumer>
+//   );
+// };
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
-
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
